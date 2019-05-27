8235e2799d2ca60c11bc8511078da84f6039dab9
SathyaReddy-Musanipalli <51062204+Sathyareddy-Musanipalli@users.noreply.github.com>
2019-05-27 08:49:35 -0700

    Add files via upload

---
+++ git-demo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 26 17:41:48 2019
+
+@author: s.musanipalli
+"""
+
+def firstpython_demo():
+    {
+     print ("this is my first program")
+     }
+    
+firstpython_demo()
